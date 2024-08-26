5e1fb1eb3319007fcf9af9fa026dac51ebbac9ae
bluseel <sahitoattique@gmail.com>
2024-08-26 16:51:17 +0500

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {
-      APIURL: "http://localhost:5000",
-    },
-  },
+  define: {},
 });
